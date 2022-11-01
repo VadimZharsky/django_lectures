8beb375875ea45e44185117640036f39e88603da
Vadim <vadimzharsky1990@gmail.com>
2022-11-01 21:19:26 +0300

    1st update

---
+++ car_dealer/cars/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+import cars
+
+app_name = 'cars'
+
+urlpatterns = [
+    path('list/', views.list, name='list'),
+    path('add/', views.add, name='add'),
+    path('delete/', views.delete, name='delete'),
+
+]
